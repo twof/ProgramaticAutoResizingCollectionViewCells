a20d956ea75dd83504b00237f257d21c3b5e1750
twof <fabiobean2@gmail.com>
2018-01-10 11:51:13 -0800

    Push for review

--- CollectionViewAutoSizingTest/Cell.swift
+++ CollectionViewAutoSizingTest/Cell.swift
@@ -9,16 +9,84 @@
 import UIKit
 
 class Cell: UICollectionViewCell {
-    @IBOutlet weak var headerLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var widthConstraint: NSLayoutConstraint!
+    var headerLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = UIFont.systemFont(ofSize: 15, weight: UIFontWeightMedium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.clearsContextBeforeDrawing = true
+        label.numberOfLines = 0
+        return label
+    }()
+
+    var descriptionLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = UIFont.systemFont(ofSize: 15, weight: UIFontWeightMedium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.clearsContextBeforeDrawing = true
+        label.numberOfLines = 0
+        return label
+    }()
+
+    let screenWidth = UIScreen.main.bounds.size.width
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        self.contentView.translatesAutoresizingMaskIntoConstraints = false
-        let screenWidth = UIScreen.main.bounds.size.width
-        widthConstraint.constant = screenWidth - (2 * 12)
     }
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints(width: 50)
+        layoutIfNeeded()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setup(width: CGFloat) {
+        descriptionLabel.text = "This is the description of the view it's kinda long in order to make sure the view gets expanded"
+        setupViews()
+        setupConstraints(width: width)
+        layoutIfNeeded()
+    }
+    
+    func setupViews() {
+        self.contentView.backgroundColor = .purple
+//        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+//        self.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(headerLabel)
+        contentView.addSubview(descriptionLabel)
+    }
+
+    func setupConstraints(width: CGFloat) {
+//        super.updateConstraints()
+
+        let widthAnchor = self.widthAnchor.constraint(equalToConstant: width)
+        widthAnchor.priority = 1000
+
+        // Self constraints
+//        NSLayoutConstraint.activate([
+//            widthAnchor
+//        ])
+
+        // Title label constraints
+        NSLayoutConstraint.activate([
+            headerLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            headerLabel.bottomAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: -8),
+            headerLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            headerLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8)
+        ])
+
+        // Description label constraints
+        NSLayoutConstraint.activate([
+            descriptionLabel.leadingAnchor.constraint(equalTo: headerLabel.leadingAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 8),
+            descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+    }
 }
--- CollectionViewAutoSizingTest/ViewController.swift
+++ CollectionViewAutoSizingTest/ViewController.swift
@@ -8,37 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     @IBOutlet weak var collectionView: UICollectionView!
     
-    let randomTexts = ["Aenean dapibus urna a ullamcorper malesuada. Ut tempor.",
-                       "Sed venenatis ligula massa, a vulputate ipsum fringilla eget. Ut justo erat, facilisis id rhoncus cursus, fringilla at.",
+    let randomTexts = [
+//        "Aenean dapibus urna a ullamcorper malesuada. Ut tempor.",
+//                       "Sed venenatis ligula massa, a vulputate ipsum fringilla eget. Ut justo erat, facilisis id rhoncus cursus, fringilla at.",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lobortis nibh metus, elementum tempus libero ornare vitae. Etiam sed leo pretium, consectetur turpis non, dapibus purus. Suspendisse potenti. Ut ut eros nunc. Cras nulla justo, porttitor non sapien at, iaculis.",
-        "Maecenas pellentesque sed magna in congue. Sed non lacus in mi posuere scelerisque. Aenean.",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget ex a velit tincidunt sodales. Donec elementum nisi at enim tempus, et rutrum erat semper. Phasellus ultricies est nec finibus."]
+//        "Maecenas pellentesque sed magna in congue. Sed non lacus in mi posuere scelerisque. Aenean.",
+//        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget ex a velit tincidunt sodales. Donec elementum nisi at enim tempus, et rutrum erat semper. Phasellus ultricies est nec finibus."
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        collectionView.register(UINib.init(nibName: "Cell", bundle: nil), forCellWithReuseIdentifier: "Cell")
+        collectionView.register(Cell.self, forCellWithReuseIdentifier: "Cell")
+//        collectionView.register(UINib.init(nibName: "Cell", bundle: nil), forCellWithReuseIdentifier: "Cell")
         if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
             flowLayout.estimatedItemSize = UICollectionViewFlowLayoutAutomaticSize
         }
         collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+    override func viewWillLayoutSubviews() {
+        collectionView.collectionViewLayout.invalidateLayout()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenWidth = UIScreen.main.bounds.size.width
+        return CGSize(width: screenWidth - 24, height: 1)
+    }
+
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return randomTexts.count
     }
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! Cell
-        cell.descriptionLabel.text = randomTexts[indexPath.row]
+        cell.headerLabel.text = randomTexts[indexPath.row]
+        cell.setup(width: collectionView.bounds.width)
         return cell
     }
 }
